d66680d580f9e95803d1de901223e6ef0552cd71
SYED-AARIJ-AHMED <saminaintekhab12@gmail.com>
2024-12-11 02:26:11 +0500

    Fix unescaped characters and other deployment issues

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@
 const nextConfig = {
   images: {
     domains: [
-      "urdzines.com",                
-      "emada.in-novation.tech",      
-      "darkskylife.com",             
-      "encrypted-tbn0.gstatic.com",  
-      "encrypted-tbn1.gstatic.com",  
-      "encrypted-tbn2.gstatic.com",  
-      "encrypted-tbn3.gstatic.com",  
+      "urdzines.com", 
+      "emada.in-novation.tech", 
+      "darkskylife.com",
+      "encrypted-tbn0.gstatic.com",
+      "encrypted-tbn1.gstatic.com",
+      "encrypted-tbn2.gstatic.com",
+      "encrypted-tbn3.gstatic.com",
     ],
   },
+  
 };
 
 module.exports = nextConfig;
